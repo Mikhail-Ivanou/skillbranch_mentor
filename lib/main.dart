afaa310be4bc98b2b989cc47bc8e694f24ee063b
Mikhail Ivanou <agidel2012@gmail.com>
2021-06-03 12:41:18 +0300

    done without test run

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+final Color _defaultColor = Color(0xFF151616);
+
 void main() {
   runApp(MyApp());
 }
@@ -28,128 +30,208 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 42,
-          ),
-          Row(
-            children: [
-              const SizedBox(
-                width: 16,
-              ),
-              Expanded(child: Center(child: Text('You'))),
-              const SizedBox(
-                width: 14,
-              ),
-              Expanded(child: Center(child: Text('Enemy'))),
-              const SizedBox(
-                width: 16,
-              ),
-            ],
-          ),
-          Spacer(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                width: 16,
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Text('Defence'.toUpperCase()),
-                    const SizedBox(
-                      height: 13,
-                    ),
-                    _BodyPartButton(
-                      bodyPart: BodyPart.head,
-                      selected: defendingBodyPart == BodyPart.head,
-                      bodyPartSetter: _selectDefendingBodyPart,
-                    ),
-                    SizedBox(
-                      height: 14,
-                    ),
-                    _BodyPartButton(
-                      bodyPart: BodyPart.torse,
-                      selected: defendingBodyPart == BodyPart.torse,
-                      bodyPartSetter: _selectDefendingBodyPart,
-                    ),
-                  ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFFE5E5E5),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 35,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 16,
                 ),
-              ),
-              const SizedBox(
-                width: 14,
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Text('Attack'.toUpperCase()),
-                    const SizedBox(
-                      height: 13,
-                    ),
-                    _BodyPartButton(
-                      bodyPart: BodyPart.head,
-                      selected: attackingBodyPart == BodyPart.head,
-                      bodyPartSetter: _selectAttackingBodyPart,
-                    ),
-                    SizedBox(
-                      height: 14,
-                    ),
-                    _BodyPartButton(
-                      bodyPart: BodyPart.torse,
-                      selected: attackingBodyPart == BodyPart.torse,
-                      bodyPartSetter: _selectAttackingBodyPart,
-                    ),
-                  ],
+                Expanded(
+                    child: Center(
+                        child: Text(
+                  'You',
+                  style: TextStyle(color: _defaultColor),
+                ))),
+                const SizedBox(
+                  width: 14,
                 ),
-              ),
-              const SizedBox(
-                width: 16,
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 14,
-          ),
-          Row(
-            children: [
-              const SizedBox(
-                width: 16,
-              ),
-              Expanded(
-                  child: SizedBox(
-                height: 40,
-                child: ColoredBox(
-                  color: Color.fromRGBO(0, 0, 0, 0.87),
-                  child: Center(
-                    child: Text(
-                      'Go'.toUpperCase(),
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.white,
+                Expanded(
+                    child: Center(
+                        child: Text(
+                  'Enemy',
+                  style: TextStyle(color: _defaultColor),
+                ))),
+                const SizedBox(
+                  width: 16,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 11,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                    child: Column(
+                  children: [
+                    _LifeWidget(),
+                    _LifeWidget(),
+                    _LifeWidget(),
+                    _LifeWidget(),
+                    _LifeWidget(),
+                  ],
+                )),
+                const SizedBox(
+                  width: 14,
+                ),
+                Expanded(
+                    child: Column(
+                  children: [
+                    _LifeWidget(),
+                    _LifeWidget(),
+                    _LifeWidget(),
+                    _LifeWidget(),
+                    _LifeWidget(),
+                  ],
+                )),
+                const SizedBox(
+                  width: 16,
+                ),
+              ],
+            ),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Text(
+                        'Defend'.toUpperCase(),
+                        style: TextStyle(color: _defaultColor),
+                      ),
+                      const SizedBox(
+                        height: 13,
+                      ),
+                      _BodyPartButton(
+                        bodyPart: BodyPart.head,
+                        selected: defendingBodyPart == BodyPart.head,
+                        bodyPartSetter: _selectDefendingBodyPart,
+                      ),
+                      const SizedBox(
+                        height: 14,
+                      ),
+                      _BodyPartButton(
+                        bodyPart: BodyPart.torso,
+                        selected: defendingBodyPart == BodyPart.torso,
+                        bodyPartSetter: _selectDefendingBodyPart,
+                      ),
+                      const SizedBox(
+                        height: 14,
+                      ),
+                      _BodyPartButton(
+                        bodyPart: BodyPart.legs,
+                        selected: defendingBodyPart == BodyPart.legs,
+                        bodyPartSetter: _selectDefendingBodyPart,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  width: 14,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Text(
+                        'Attack'.toUpperCase(),
+                        style: TextStyle(color: _defaultColor),
+                      ),
+                      const SizedBox(
+                        height: 13,
+                      ),
+                      _BodyPartButton(
+                        bodyPart: BodyPart.head,
+                        selected: attackingBodyPart == BodyPart.head,
+                        bodyPartSetter: _selectAttackingBodyPart,
+                      ),
+                      const SizedBox(
+                        height: 14,
+                      ),
+                      _BodyPartButton(
+                        bodyPart: BodyPart.torso,
+                        selected: attackingBodyPart == BodyPart.torso,
+                        bodyPartSetter: _selectAttackingBodyPart,
+                      ),
+                      const SizedBox(
+                        height: 14,
+                      ),
+                      _BodyPartButton(
+                        bodyPart: BodyPart.legs,
+                        selected: attackingBodyPart == BodyPart.legs,
+                        bodyPartSetter: _selectAttackingBodyPart,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: _isGoEnabled ? processGo : null,
+                    child: SizedBox(
+                      height: 40,
+                      child: ColoredBox(
+                        color: _isGoEnabled ? Color(0xFF000000) : Color(0x61000000),
+                        child: Center(
+                          child: Text(
+                            'Go'.toUpperCase(),
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w900,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              )),
-              const SizedBox(
-                width: 16,
-              ),
-            ],
-          )
-        ],
+                const SizedBox(
+                  width: 16,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  bool get _isGoEnabled => defendingBodyPart != null && attackingBodyPart != null;
 
   void _selectDefendingBodyPart(final BodyPart value) {
     setState(() {
@@ -161,6 +243,27 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       attackingBodyPart = value;
     });
+  }
+
+  void processGo() {
+    setState(() {
+      attackingBodyPart = null;
+      defendingBodyPart = null;
+    });
+  }
+}
+
+class _LifeWidget extends StatelessWidget {
+  const _LifeWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '1',
+      style: TextStyle(color: _defaultColor),
+    );
   }
 }
 
@@ -183,9 +286,12 @@ class _BodyPartButton extends StatelessWidget {
       child: SizedBox(
         height: 40,
         child: ColoredBox(
-          color: selected ? Color(0xFF1C79CE) : Color(0xFFD6D6D6),
+          color: selected ? Color(0xFF1C79CE) : Color(0x61000000),
           child: Center(
-            child: Text(bodyPart.name.toUpperCase()),
+            child: Text(
+              bodyPart.name.toUpperCase(),
+              style: TextStyle(color: selected ? Colors.white : Color(0xFF060D14)),
+            ),
           ),
         ),
       ),
@@ -199,7 +305,8 @@ class BodyPart {
   const BodyPart._(this.name);
 
   static const head = BodyPart._('Head');
-  static const torse = BodyPart._('Torse');
+  static const torso = BodyPart._('Torso');
+  static const legs = BodyPart._('Legs');
 
   @override
   String toString() {
